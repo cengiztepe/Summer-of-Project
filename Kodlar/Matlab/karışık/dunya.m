531196c646266504ef8820b80d6fb64af51e687c
unknown <omer.bulamac@gmail.com>
2012-07-03 16:35:07 +0300

    dunyan 3d gorunumu

---
+++ Kodlar/Matlab/karışık/dunya.m
@@ -0,0 +1,9 @@
+load earth % Load image data, X, and colormap, map
+sphere; h = findobj('Type','surface');
+hemisphere = [ones(257,125),... 
+              X,... 
+              ones(257,125)];
+set(h,'CData',flipud(hemisphere),'FaceColor','texturemap')
+colormap(map)
+axis equal
+view([90 0])
